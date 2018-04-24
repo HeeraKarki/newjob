d6004f71cd7682816546702a30373f929c7fe298
bkjonathan <admin@sinphyukyun.com>
2018-04-24 23:38:53 +0630

    Employer Profile Beta 1

--- app/view/user/empolyer/profile.view.php
+++ app/view/user/empolyer/profile.view.php
@@ -13,7 +13,36 @@ view_require('applyjob/nav');
             <h2 class="title">My Profile</h2>
         </div><!-- breadcrumb-section -->
 
-        <?php view_require('applyjob/employer_profile_head');?>
+        <div class="job-profile section" style="box-shadow: 2px 9px 7px rgba(249,219,255,0.43);">
+            <div class="user-profile">
+                <div class="user-images">
+                    <!--                    <img src="--><?//= asset('applyjob/')?><!--images/user.jpg" alt="User Images" class="img-responsive">-->
+                    <img width="100" src="<?= isset($user->employer->avatar)?$user->employer->avatar!==NULL?asset($user->employer->avatar):'':asset('applyjob/images/user.jpg') ?>" alt="User Images" class="img-responsive">
+                </div>
+                <div class="user">
+                    <h2 class="text-capitalize">Hello, <a href=""><?= auth()['name'] ?></a></h2>
+                    <h5><?= auth()['email'] ?></h5>
+                </div>
+
+                <div class="favorites-user">
+                    <div class="my-ads">
+                        <a href="applied-job.php">29<small>Apply Job</small></a>
+                    </div>
+                    <div class="favorites">
+                        <a href="bookmark.php"><?=  $user->employer->post_count ?><small>Post Left</small></a>
+                    </div>
+                </div>
+            </div><!-- user-profile -->
+
+            <ul class="user-menu">
+                <li ><a href="<?= baseurl('User/Employer') ?>">Account Info </a></li>
+                <li><a href="<?= baseurl('Employer/Job_List')?>">My Job List</a></li>
+                <li class="active"><a href="<?= baseurl('Employer/Profile_Detail') ?>">Profile Details</a></li>
+                <li><a href="<?= asset('User/Applied_Job')?>">applied job</a></li>
+                <li><a href="<?= baseurl('Employer/Packages')?>">Buy Post Packages</a></li>
+                <li><a href="<?= baseurl('User/Delete') ?>">Close account</a></li>
+            </ul>
+        </div>
         <?php view_require('applyjob/success');?>
         <?php view_require('applyjob/error');?>
 
